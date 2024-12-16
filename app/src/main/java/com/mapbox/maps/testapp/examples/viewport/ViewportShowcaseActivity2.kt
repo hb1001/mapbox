97903b7bfbfc3fd880d29d3b07734500c37ca4d3
zhangddq <1941670713@qq.com>
2024-12-17 01:39:06 +0800

    1

--- app/src/main/java/com/mapbox/maps/testapp/examples/viewport/ViewportShowcaseActivity2.kt
+++ app/src/main/java/com/mapbox/maps/testapp/examples/viewport/ViewportShowcaseActivity2.kt
@@ -1,37 +1,20 @@
 package com.mapbox.maps.testapp.examples.viewport
 
-import android.annotation.SuppressLint
-import android.content.Intent
-import android.content.res.Configuration
 import android.content.res.Resources
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
-import com.mapbox.api.directions.v5.models.DirectionsResponse
-import com.mapbox.core.constants.Constants
-import com.mapbox.geojson.LineString
-import com.mapbox.geojson.Point
 import com.mapbox.maps.CameraOptions
 import com.mapbox.maps.EdgeInsets
 import com.mapbox.maps.ImageHolder
-import com.mapbox.maps.logI
 import com.mapbox.maps.plugin.LocationPuck2D
 import com.mapbox.maps.plugin.locationcomponent.location
-import com.mapbox.maps.plugin.viewport.ViewportPlugin
 import com.mapbox.maps.plugin.viewport.data.FollowPuckViewportStateOptions
-import com.mapbox.maps.plugin.viewport.data.OverviewViewportStateOptions
 import com.mapbox.maps.plugin.viewport.state.FollowPuckViewportState
-import com.mapbox.maps.plugin.viewport.state.OverviewViewportState
 import com.mapbox.maps.plugin.viewport.viewport
 import com.mapbox.maps.testapp.R
 import com.mapbox.maps.testapp.databinding.ActivityViewportAnimationBinding
-import com.mapbox.maps.testapp.examples.annotation.AnnotationUtils
-import com.mapbox.maps.testapp.utils.SimulateRouteLocationProvider
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import android.location.Location
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.LocationCallback
@@ -49,7 +32,7 @@ class ViewportShowcaseActivity2 : AppCompatActivity() {
   private val pixelDensity = Resources.getSystem().displayMetrics.density
   private val followingPadding: EdgeInsets by lazy {
     EdgeInsets(
-      180.0 * pixelDensity,
+      380.0 * pixelDensity,
       40.0 * pixelDensity,
       150.0 * pixelDensity,
       40.0 * pixelDensity
@@ -66,30 +49,37 @@ class ViewportShowcaseActivity2 : AppCompatActivity() {
 
     // 启用实时定位
     startLocationUpdates(binding.mapView)
-
-
   }
 
   private fun startLocationUpdates(mapView: MapView) {
+    // 可以删除
+    mapView.getMapboxMap().setCamera(CameraOptions.Builder().pitch(0.0).build())
+
     // 定位请求参数
     val locationRequest = LocationRequest.create().apply {
       interval = 1000 // 更新间隔 (1秒)
       fastestInterval = 500 // 最快间隔
       priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+      smallestDisplacement = 1.0f
     }
 
     // 监听定位结果
     locationCallback = object : LocationCallback() {
       override fun onLocationResult(locationResult: LocationResult) {
         super.onLocationResult(locationResult)
-//        for (location in locationResult.locations) {
+        for (location in locationResult.locations) {
 //          updateLocationOnMap(mapView, location)
-//        }
+          Toast.makeText(this@ViewportShowcaseActivity2, location.toString(), Toast.LENGTH_SHORT).show()
+        }
+
       }
     }
+
+    // 视角跟随
     followPuckViewportState =mapView.viewport.makeFollowPuckViewportState(
       FollowPuckViewportStateOptions.Builder()
         .pitch(0.0)
+        .padding(followingPadding)
         .build()
     )
     mapView.location.updateSettings {
@@ -98,41 +88,15 @@ class ViewportShowcaseActivity2 : AppCompatActivity() {
       )
       puckBearingEnabled = true // 启用图标方向跟随
     }
-
-    setupViewportPlugin(mapView.viewport)
+    viewportButton.setOnClickListener {
+      mapView.viewport.transitionTo(followPuckViewportState)
+    }
     // 开始请求位置更新
     fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, mainLooper)
-  }
-
-  private fun updateLocationOnMap(mapView: MapView, location: Location) {
-    val point = Point.fromLngLat(location.longitude, location.latitude)
-    mapView.mapboxMap.setCamera(
-      CameraOptions.Builder()
-        .center(point) // 将摄像头移动到用户位置
-        .bearing(location.bearing.toDouble()) // 根据用户方向设置地图旋转角度
-        .zoom(15.0) // 放大级别
-        .build()
-    )
-
-
   }
 
   override fun onDestroy() {
     super.onDestroy()
     fusedLocationProviderClient.removeLocationUpdates(locationCallback)
-  }
-
-  private fun setupViewportPlugin(viewport: ViewportPlugin) {
-    // 初始化跟随和概览视图状态
-    followPuckViewportState =
-      viewport.makeFollowPuckViewportState(FollowPuckViewportStateOptions.Builder().build())
-    // 根据屏幕方向设置不同的 padding
-    followPuckViewportState.options = followPuckViewportState.options.toBuilder()
-      .padding(followingPadding).build()
-
-    // 为按钮绑定点击事件
-    viewportButton.setOnClickListener {
-      viewport.transitionTo(followPuckViewportState)
-    }
   }
 }
